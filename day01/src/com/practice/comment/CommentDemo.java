3cce424c7f88ddd0cb4caeed86ea160b45e050db
如影随形 <1009253766@qq.com>
2024-10-18 16:45:12 +0800

    day01
    java基础语法

---
+++ day01/src/com/practice/comment/CommentDemo.java
@@ -0,0 +1,18 @@
+package com.practice.comment;
+
+/** 文档注释
+ 这是一个用来讲解注释的使用的
+ */
+public class CommentDemo {
+    public static void main(String[] args) {
+        // 单行注释
+        // 打印2行诗句
+        System.out.println("我欲乘风归去，又恐琼楼玉宇");
+
+        /*
+          多行注释
+          打印3行诗句，依次打印
+         */
+        System.out.println("我欲乘风归去，又恐琼楼玉宇");
+    }
+}
